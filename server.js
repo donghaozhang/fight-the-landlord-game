5f2836eb24dee2ba5c88d6bbea6ffbe24598bf8c
Donghao Zhang <zdhpeter1991@gmail.com>
2025-05-30 11:13:52 +1000

    Add basic server tests

--- server.js
+++ server.js
@@ -4,34 +4,47 @@ const path = require('path');
 
 const PORT = process.env.PORT || 8080;
 
-const server = http.createServer((req, res) => {
-  let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
-  const ext = path.extname(filePath);
-  let contentType = 'text/html';
+function createServer() {
+  return http.createServer((req, res) => {
+    let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
+    const ext = path.extname(filePath);
+    let contentType = 'text/html';
 
-  switch (ext) {
-    case '.js':
-      contentType = 'text/javascript';
-      break;
-    case '.css':
-      contentType = 'text/css';
-      break;
-    case '.json':
-      contentType = 'application/json';
-      break;
-  }
-
-  fs.readFile(filePath, (err, content) => {
-    if (err) {
-      res.writeHead(404);
-      res.end('Not Found');
-    } else {
-      res.writeHead(200, { 'Content-Type': contentType });
-      res.end(content, 'utf-8');
+    switch (ext) {
+      case '.js':
+        contentType = 'text/javascript';
+        break;
+      case '.css':
+        contentType = 'text/css';
+        break;
+      case '.json':
+        contentType = 'application/json';
+        break;
     }
-  });
-});
 
-server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+    fs.readFile(filePath, (err, content) => {
+      if (err) {
+        res.writeHead(404);
+        res.end('Not Found');
+      } else {
+        res.writeHead(200, { 'Content-Type': contentType });
+        res.end(content, 'utf-8');
+      }
+    });
+  });
+}
+
+const server = createServer();
+
+function startServer(port = PORT, callback) {
+  return server.listen(port, () => {
+    if (callback) callback();
+    console.log(`Server running on http://localhost:${port}`);
+  });
+}
+
+if (require.main === module) {
+  startServer();
+}
+
+module.exports = { server, startServer, createServer };
